1b09f346c590795796a6f3caa4918644cb66e83a
mikima <mikimauri@gmail.com>
2017-01-24 16:31:23 +0100

    YO

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var size, energy=0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+  translate(width/2,height/2);
+  ellipse(0,0,energy);
+}
+
+function deviceShaken(){
+  energy += abs(accelerationX) + abs(accelerationY) + abs(accelerationZ);
+}
